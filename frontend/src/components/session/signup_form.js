cc3047bc303eb3434c197c8079a250bfcf7dda15
Elson Li <elson.li@berkeley.edu>
2020-01-21 00:15:04 -0800

    Add renderErrors to 'signup_form.js'

--- frontend/src/components/session/signup_form.js
+++ frontend/src/components/session/signup_form.js
@@ -9,6 +9,7 @@ class SignupForm extends React.Component {
       password: ""
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.renderErrors = this.renderErrors.bind(this);
   }
 
   update(field) {
@@ -22,6 +23,20 @@ class SignupForm extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
     this.props.signup(this.state);
+  }
+
+  renderErrors() {
+    return (
+      <ul>
+        {
+          this.props.errors.map((error, idx) => {
+            return (
+              <li key={ `error-${ idx }` }>{ error }</li>
+            );
+          })
+        }
+      </ul>
+    );
   }
 
   render() {
@@ -42,6 +57,7 @@ class SignupForm extends React.Component {
               onChange={ this.update("password") }
               placeholder="Password"
             />
+            <br />
             <input type="submit" value="Submit" />
           </div>
         </form>

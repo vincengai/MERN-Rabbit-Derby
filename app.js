4f940a74e0692a10d0257352174e993bcdf31539
Helen Cheung <helenkei.cheung@gmail.com>
2020-01-22 02:00:46 -0800

    wrote highscores backend routes & models - no custom validations

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 
 const users = require("./routes/api/users");
+const highscores = require('./routes/api/highscores');
 
 const path = require("path");
 
@@ -33,6 +34,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api/users", users);
+app.use('/api/highscores', highscores);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));

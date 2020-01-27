cfb0f81600210a1a9e12053182cd4fa9c8aded03
Elson Li <elson.li@berkeley.edu>
2020-01-27 00:58:20 -0800

    heroku attempt 10

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const usermaps = require('./routes/api/usermaps');
 const path = require("path");
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static(path.join(__dirname, "frontend/build")));
   app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
